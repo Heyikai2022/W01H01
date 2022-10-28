dc5786d2b888e91b4d3d3e0b1da2216b6f8d4e69
Yikai He <ge48hir@in.tum.de>
2022-10-28 17:35:42 +0200

    finish

---
+++ src/hallo/hello.java
@@ -0,0 +1,8 @@
+package hallo;
+
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Süßer Pingu!");
+    }
+}
